2060a46b4233de48f87e2d53695169beab0cb110
lit-alan <Alan.Ryan@lit.ie>
2023-09-11 20:52:58 +0100

    initial commit

---
+++ Source For Lectures/L1 Introduction to Java Server Pages (JSP's)/EmailAddressAppV2/src/main/java/src/java/data/UserIO.java
@@ -0,0 +1,23 @@
+package src.java.data;
+
+
+import src.java.business.User;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class UserIO {
+    
+    public static void add(User user, String filepath) throws IOException {
+
+        File file = new File(filepath);
+        PrintWriter out = new PrintWriter(new FileWriter(file, true));
+        out.println(user.getEmailAddress() + "|" + user.getFirstName() + "|" + user.getLastName());
+        out.close();
+        
+    }
+}
+
